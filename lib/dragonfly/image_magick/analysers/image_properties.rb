d67df382e90b38da83d6d3b430d99e62065c76d3
Scott Arthur <scott@scottatron.com>
2014-05-01 12:56:29 +1000

    Added exception handling to ImageMagick image_properties analyser as the handling in the image analyser doesn't seem to work

--- lib/dragonfly/image_magick/analysers/image_properties.rb
+++ lib/dragonfly/image_magick/analysers/image_properties.rb
@@ -5,15 +5,19 @@ module Dragonfly
 
         def call(content)
           identify_command = content.env[:identify_command] || 'identify'
-          details = content.shell_eval do |path|
-            "#{identify_command} -ping -format '%m %w %h' #{path}"
+          begin
+            details = content.shell_eval do |path|
+              "#{identify_command} -ping -format '%m %w %h' #{path}"
+            end
+            format, width, height = details.split
+            {
+              'format' => format.downcase,
+              'width' => width.to_i,
+              'height' => height.to_i
+            }
+          rescue Dragonfly::Shell::CommandFailed
+            {'format' => ''}
           end
-          format, width, height = details.split
-          {
-            'format' => format.downcase,
-            'width' => width.to_i,
-            'height' => height.to_i
-          }
         end
 
       end
